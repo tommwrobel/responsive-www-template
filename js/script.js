b6c6f37b0a2843a1e43c39f540ef6f97e9fd5145
Tomek <tomm.wrobel@gmail.com>
2020-10-21 13:09:09 +0200

    Added mobile menu

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ window.addEventListener('scroll', function(){
     navbar.classList.toggle("navbar-sticky", window.scrollY > 0);
 });
 
+window.addEventListener('resize', function(){
+    const nav = document.getElementsByClassName('mobile-navigation')[0];
+    nav.classList.remove("mobile-navigation-active");
+});
 function toggleMenu() {
     //const menuToggle = document.getElementsByClassName('menuToggle')[0];
-    const nav = document.getElementsByClassName('main-navigation')[0];
+    const nav = document.getElementsByClassName('mobile-navigation')[0];
     //menuToggle.classList.toggle("toggle-active");
-    nav.classList.toggle("main-navigation-active");  
+    nav.classList.toggle("mobile-navigation-active");
 }
