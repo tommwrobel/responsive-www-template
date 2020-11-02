b902384ab96dbe2a48e4b35a4feb313f36f2fd78
Tomasz Wróbel <tomm.wrobel@gmail.com>
2020-11-03 00:26:28 +0100

    Redesigning page

--- js/script.js
+++ js/script.js
@@ -1,15 +1,41 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const navbar = document.getElementsByClassName('navbar')[0];
+//     if (navbar != null) {
+//         document.body.style.paddingTop = navbar.clientHeight + "px";
+//     }
+// }, false);
+
 window.addEventListener('scroll', function(){
     const navbar = document.getElementsByClassName('navbar')[0];
-    navbar.classList.toggle("navbar-sticky", window.scrollY > 0);
+    navbar.classList.toggle("navbar-sticky", window.scrollY > 8);
 });
 
-window.addEventListener('resize', function(){
-    const nav = document.getElementsByClassName('mobile-navigation')[0];
-    nav.classList.remove("mobile-navigation-active");
-});
+if (document.getElementsByClassName("nav-main-items")[0]) {
+    document.getElementsByClassName("nav-main-items")[0].addEventListener("click", toggleMenu); 
+}
+
 function toggleMenu() {
-    //const menuToggle = document.getElementsByClassName('menuToggle')[0];
-    const nav = document.getElementsByClassName('mobile-navigation')[0];
-    //menuToggle.classList.toggle("toggle-active");
-    nav.classList.toggle("mobile-navigation-active");
+    const nav = document.getElementsByClassName('nav-main-items')[0];
+    if (nav) {
+        console.log("work");
+        nav.classList.toggle("mobile-navigation-visible");
+    }
+}
+
+// var slider = tns({
+//     container: '.slider-items',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: true,
+//     autoplayTimeout: 4000,
+//     speed: 500,
+//     autoplayButtonOutput: false,
+//     controls: false,
+// });
+
+function toggleFaqItem(element) {
+    const faqItem = element.parentNode.children[1];
+    const faqArrow = element.children[1];
+    faqArrow.classList.toggle("faq-arrow-open");
+    faqItem.classList.toggle("faq-content-open");
 }
